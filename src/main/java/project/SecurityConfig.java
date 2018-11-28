b5c1a603a8246ba3df46d769be438d1515794856
Kristján Petur Þórarinsson <kth130@hi.is>
2018-11-28 14:17:16 +0000

    Allow resources to be published

--- src/main/java/project/SecurityConfig.java
+++ src/main/java/project/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -55,5 +56,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureUrl("/login?error=true")
                 .and()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/");
+    }
+
+    /**
+     *
+     * Allows all utils, resources, css, jsp files and images.
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/webapp/**", "/img/**");
     }
 }
